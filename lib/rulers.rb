34cfb1d4351c88c241a83941b0945a75c4dad7c1
JanBussieck <jan.bussieck@gmail.com>
2014-09-06 00:58:41 +0200

    Added ruler config and initialized an best_quotes, added a first test

---
+++ lib/rulers.rb
@@ -0,0 +1,12 @@
+require "rulers/version"
+require "rulers/array"
+module Rulers
+  
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from Ruby on Rulers!"]]
+    end
+  end
+  # Your code goes here...
+end
---
+++ test/test_application.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper'
+
+class TestApp < Rulers::Application
+end
+
+class RulersAppTest < Test::Unit::TestCase
+  
+  def app 
+    TestApp.new
+  end
+  
+  def test_request
+    get "/"
+    
+    assert last_response.ok?
+    body = last_response.body
+    assert body["Hello"]
+  end
+  
+end
